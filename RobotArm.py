36c195ea2e0ec9840646046ff8bcab195154fcc8
Travis Bowers <trabower@umich.edu>
2014-11-21 14:57:35 -0500

    fleshed out some member data

--- RobotArm.py
+++ RobotArm.py
@@ -1,2 +1,17 @@
 #Robot Arm Model
 #This file will contain an object oriented implementation of a drawing robot class
+
+class GreenRobotArm ( RobotSimInterface ):
+    def __init__(self, *args, **kw):
+        RobotSimInterface.__init__(self, *args, **kw)
+        '''
+        might we want to initialize these to be pointing in the positive y direction?
+        '''
+        self.L1 = 
+        self. L2 = 
+        self.L3 =
+        self.shoulderRotation=
+        self.shoulderAngle =
+        self.elbowAngle = 
+        self.writsTwist =
+        self.wristBend
